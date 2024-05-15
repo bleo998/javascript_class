e076c83ebc4e99d56355cb4de69082d3d666fedc
bleo998 <hop2k13@gmail.com>
2024-05-15 22:10:37 +0200

    Attached files

---
+++ App.js
@@ -0,0 +1,21 @@
+console.log('test');
+
+let stringVar = "Hello World!";
+console.log(typeof stringVar);
+
+let numVar = 404808;
+console.log(typeof numVar);
+
+let booleanVar = true;
+console.log(typeof booleanVar);
+
+let fruits = ["banana", "apple", "cherry", "orange"];
+console.log(typeof fruits);
+
+let faveCar = {
+    name: "Audi",
+    year: 2024,
+    gearBox: "manual"
+}
+
+console.log(typeof faveCar)
